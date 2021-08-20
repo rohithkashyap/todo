3bf52d347c6a438396e5e9e6198f3a27bee4b593
Rohith Kashyap <rohithkashyap25@gmail.com>
2021-08-20 18:21:23 -0400

    first impl

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form @submit.prevent="addToList()">
+      <input type="text" v-model="newTodoItem" /><br /><br />
+      <button>Add</button>
+    </form>
+    <ol>
+      <todo-item
+        v-for="todo in todoList"
+        v-bind:todo="todo"
+        v-bind:key="todo.id"
+        v-on:remove-item="removeItem"
+      ></todo-item>
+    </ol>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from "./components/TodoItem.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TodoItem,
+  },
+  data() {
+    return {
+      todoList: [],
+      newTodoItem: "",
+      nextIndex: 0,
+    };
+  },
+  methods: {
+    addToList() {
+      this.todoList.push({
+        id: this.nextIndex,
+        text: this.newTodoItem,
+      });
+      this.newTodoItem = "";
+      console.log(this.nextIndex);
+      this.nextIndex++;
+    },
+    removeItem(idx) {
+      this.todoList = this.todoList.filter(function (obj) {
+        return obj.id !== idx;
+      });
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li> {{ todo.text }} <button v-on:click="remove()"> Remove </button> </li>
+  
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['todo'],
+  methods: {
+    remove() {
+      this.$emit("remove-item", this.todo.id)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
